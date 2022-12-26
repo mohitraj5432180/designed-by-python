2e7705ac18e4f3c61036d0c7e1a2bed6acc8078a
mohitraj5432180 <119477052+mohitraj5432180@users.noreply.github.com>
2022-12-26 14:23:33 +0530

    Add files via upload

---
+++ cool-shape.py
@@ -0,0 +1,19 @@
+import turtle
+import colorsys
+t= turtle.Turtle()
+turtle.Screen().bgcolor("black")
+t.pensize(5)
+t.speed(0)
+n=100
+h=0
+for i in range (90):
+    c=colorsys.hsv_to_rgb(h,1,0,8)
+    h+=1/n
+    t.color(c)
+    t.left(250)
+    t.forward(1*2)
+    t.right(40)
+    t.backward(1*3)
+    t.circle(70,100)
+    t.done()
+
